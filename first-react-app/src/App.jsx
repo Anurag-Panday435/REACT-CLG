6c27ddd69c86c4d262a1248b74e5eb4373a9b534
Anurag Pandey <anuragpandey457635@gmail.com>
2026-05-25 14:08:49 +0530

    FirstREact

---
+++ first-react-app/src/App.jsx
@@ -0,0 +1,63 @@
+// import {useState} from 'react'
+
+// const App = () => {
+//   let [count,SetCount]=  useState(0)
+//   function fun1(){
+//     SetCount(count+1)
+//   }
+//   return (
+//     <div>
+//       <h3>{count}</h3>
+//       <button onClick={fun1}>click </button>
+//     </div>
+//   )
+// }
+
+// export default App
+//-------------------------------------------------------------------
+// import React, {useState} from 'react'
+
+// const App = () => {
+//   let[color,SetColor]= useState("red")
+//   function fun1(){
+//     SetColor("green")
+//   }
+//   function fun2(){
+//     SetColor("black")
+//   }
+
+//   return (
+//     <div style={{backgroundColor:color,height:"100vh"}}>
+//       <button onClick={fun1}>green</button>
+//         <button onClick={fun2}>black</button>
+//     </div>
+//   )
+// }
+
+// export default App
+//---------------------------------------------------------------------
+
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div><Home/></div>
+//   )
+// }
+// const Home =() =>{
+//   return (
+//     <div>Hellooooo</div>
+//   )
+// }
+// export default App
+//-----------------------------------------------------
+import React from 'react'
+import Home from './Home'
+
+const App = () => {
+  return (
+    <div>Home</div>
+  )
+}
+export default App
+rafce
